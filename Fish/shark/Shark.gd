6d45380fe2fec242fb3fcbd5ed8c71d2842a2617
richardcoldwell74 <76210848+richardcoldwell74@users.noreply.github.com>
2022-01-21 00:25:14 +0000

    Start on shark and crab scenes

---
+++ Fish/shark/Shark.gd
@@ -0,0 +1,16 @@
+extends Area2D
+
+
+var velocity: Vector2 = Vector2(50, 0)
+
+
+func _ready():
+	$AnimationPlayer.play("swimming")
+
+
+func _physics_process(delta) -> void:
+	position -= velocity * delta  
+
+
+func fish_caught()-> void:
+	$AnimationPlayer.play("caught")
